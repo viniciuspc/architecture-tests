1e9922e3ed35fc91c47e22fecd92f2292cffd14b
viniciuspc <viniciuspc@vinicius-pc>
2019-11-06 11:15:27 +0000

    Make some changes to be possible to run on the apache-flink as a jar

--- src/main/java/pt/archifeed/flink/ConnectionTest.java
+++ src/main/java/pt/archifeed/flink/ConnectionTest.java
@@ -2,8 +2,10 @@ package pt.archifeed.flink;
 
 import java.util.List;
 
+import org.apache.flink.api.java.CollectionEnvironment;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.utils.ParameterTool;
 
 /**
  * Make a simple connection with apache flink and make operations on data.
@@ -13,17 +15,22 @@ import org.apache.flink.api.java.ExecutionEnvironment;
 public class ConnectionTest {
 	
 	public static void main(String[] args) throws Exception {
-		ExecutionEnvironment env = ExecutionEnvironment.createRemoteEnvironment("localhost", 8081);
+		final ParameterTool params = ParameterTool.fromArgs(args);
+		
+		//ExecutionEnvironment env = CollectionEnvironment.createRemoteEnvironment("localhost", 8081);
+		ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+		env.getConfig().setGlobalJobParameters(params);
+		
 		DataSet<Integer> amounts = env.fromElements(1, 29,40, 50);
 		
 		int threshold = 30;
 		List<Integer> collect = amounts
-				  .filter(a -> a > threshold)
-				  .reduce((integer, t1) -> integer + t1)
-				  .collect();
+				 .filter(a -> a > threshold)
+				 .reduce((integer, t1) -> integer + t1)
+				
+				.collect();
 		
 		System.out.println(collect);
-		
 	}
 
 }
