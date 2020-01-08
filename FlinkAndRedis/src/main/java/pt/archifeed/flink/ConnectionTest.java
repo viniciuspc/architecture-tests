ad09800977e74612102f60c586200e5a42073146
viniciuspc <viniciuspc@vinicius-pc>
2020-01-08 11:17:19 +0000

    Included examples of time windows and implemented ML mapper.
    - Created an example of using the time window.
    - Created class ApplyMLMapper to apply the machine learning model on the
    workflow of apache flink
    - Include the randomForest.model file that has the serialized model
    class used on the classification.

--- FlinkAndRedis/src/main/java/pt/archifeed/flink/ConnectionTest.java
+++ FlinkAndRedis/src/main/java/pt/archifeed/flink/ConnectionTest.java
@@ -1,11 +1,20 @@
 package pt.archifeed.flink;
 
-import java.util.List;
-
-import org.apache.flink.api.java.CollectionEnvironment;
-import org.apache.flink.api.java.DataSet;
-import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+
+import pt.archifeed.flink.TransactionProcesser.HourlyExtractor;
+import pt.archifeed.flink.TransactionProcesser.SumAggregate;
+import pt.archifeed.flink.MapFunctions.EnrichMapper;
+import pt.archifeed.flink.model.TransactionModel;
 
 /**
  * Make a simple connection with apache flink and make operations on data.
@@ -18,19 +27,48 @@ public class ConnectionTest {
 		final ParameterTool params = ParameterTool.fromArgs(args);
 		
 		//ExecutionEnvironment env = CollectionEnvironment.createRemoteEnvironment("localhost", 8081);
-		ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
-		env.getConfig().setGlobalJobParameters(params);
+		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 		
-		DataSet<Integer> amounts = env.fromElements(1, 29,40, 50);
+//		DataSet<Integer> amounts = env.fromElements(1, 29,40, 50);
+//		
+//		int threshold = 30;
+//		List<Integer> collect = amounts
+//				 .filter(a -> a > threshold)
+//				 .reduce((integer, t1) -> integer + t1)
+//				
+//				.collect();
+//		
+//		System.out.println(collect);
 		
-		int threshold = 30;
-		List<Integer> collect = amounts
-				 .filter(a -> a > threshold)
-				 .reduce((integer, t1) -> integer + t1)
-				
-				.collect();
+		DataStream<String> text = env.readTextFile("text-files/timewindowtest.csv");
 		
-		System.out.println(collect);
+		SingleOutputStreamOperator<TransactionModel> transactionModels = text
+																																				.map(new MapFunction<String, TransactionModel>() {
+
+																																					@Override
+																																					public TransactionModel map(String value) throws Exception {
+																																						String[] fields = value.split(",");
+																																						return new TransactionModel(fields);
+																																						
+																																					}
+																																				})
+																																				.assignTimestampsAndWatermarks(new HourlyExtractor(Time.hours(1)));
+		//transactionModels.print();
+		
+		SingleOutputStreamOperator<Tuple2<String, Double>> aggregate = transactionModels
+				.keyBy((transaction) -> transaction.getNameOrig())
+				.window(SlidingEventTimeWindows.of(Time.hours(2),Time.hours(1)))
+				.aggregate(new SumAggregate());
+		
+		aggregate.print();
+		
+		
+		
+		env.execute();
+		
+		
+		
 	}
 
 }
--- FlinkAndRedis/src/main/java/pt/archifeed/flink/TransactionProcesser.java
+++ FlinkAndRedis/src/main/java/pt/archifeed/flink/TransactionProcesser.java
@@ -1,37 +1,33 @@
 package pt.archifeed.flink;
 
-import java.time.Duration;
 import java.time.LocalTime;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Vector;
 
-import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
-import org.apache.flink.configuration.Configuration;
-import org.apache.flink.metrics.Meter;
-import org.apache.flink.metrics.MetricGroup;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.redis.RedisSink;
 import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig;
 import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommand;
 import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommandDescription;
 import org.apache.flink.streaming.connectors.redis.common.mapper.RedisMapper;
-import org.json.JSONObject;
 
 import pt.archifeed.encription.AES;
 import pt.archifeed.encription.AES128;
 import pt.archifeed.encription.AES256;
+import pt.archifeed.flink.MapFunctions.ApplyMLMapper;
 import pt.archifeed.flink.MapFunctions.EnrichMapper;
 import pt.archifeed.flink.MapFunctions.JsonEncryptMapper;
 import pt.archifeed.flink.MapFunctions.RulesMapper;
-import pt.archifeed.flink.Metrics.CustomDropwizardMeterWrapper;
-import pt.archifeed.flink.Metrics.LatencyMeter;
 import pt.archifeed.flink.model.TransactionModel;
-import redis.clients.jedis.Jedis;
+import weka.core.SerializationHelper;
 
 public class TransactionProcesser {
 	
@@ -39,19 +35,17 @@ public class TransactionProcesser {
 		
 		ParameterTool params = ParameterTool.fromArgs(args);
 		
-		//FlinkJedisPoolConfig conf = new FlinkJedisPoolConfig.Builder().setHost("172.17.0.4").setPort(6379).build();
-		
 		FlinkJedisPoolConfig conf = new FlinkJedisPoolConfig.Builder().setHost(params.get("transactions-host")).setPort(6379).build();
 		String decisionsRulesHost = params.get("decisions-rules-host");
 		String enrichmentHost = params.get("enrichment-host");
+		String secret = params.get("secret");
+		String aesType = params.get("AES");
+		Vector v = (Vector) SerializationHelper.read(params.get("model-file-path"));
 		
 		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 		env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 		
-		//env.setParallelism(1);
-		//String secret = "h.uzZJ$j3S^jHV";
-		String secret = params.get("secret");
-		String aesType = params.get("AES");
+		
 		
 		AES aes = null;
 		if(secret != null && !secret.isEmpty() && aesType != null && !aesType.isEmpty()) {
@@ -69,15 +63,20 @@ public class TransactionProcesser {
 		DataStream<String> text = env.readTextFile(params.get("file-path"));
 		
 		//2 - Convert text into Object while enrich it with the country information. Output TransactionModel
-		SingleOutputStreamOperator<TransactionModel> transactionModels = text.map(new EnrichMapper(enrichmentHost));
+		SingleOutputStreamOperator<TransactionModel> transactionModels = text.map(new EnrichMapper(enrichmentHost)).assignTimestampsAndWatermarks(new HourlyExtractor(Time.hours(1)));
 		//3 - Make a agregation in a time window and sum the amount. Otput ...
-		//transactionModels.keyBy((transaction) -> transaction.getNameOrig()).timeWindow(Time.hours(2)).reduce((a,b) -> a.getAmount()+b.getAmount());
+		SingleOutputStreamOperator<Tuple2<String, Double>> aggregate = transactionModels
+										.keyBy((transaction) -> transaction.getNameOrig())
+										.window(TumblingEventTimeWindows.of(Time.hours(2)))
+										.aggregate(new SumAggregate());
 		
 		//4 - apply rules - country and sum of the amunt Output TransactionModel
 		SingleOutputStreamOperator<TransactionModel> transactionModelsWithRules = transactionModels.map(new RulesMapper(decisionsRulesHost));
-		//5 - Convert to json and encrypt if is nescessary. Output Tuple2<String,String>
-		SingleOutputStreamOperator<Tuple2<String,String>> tuples = transactionModelsWithRules.map(new JsonEncryptMapper(initialTime, aes));
-		//6 - sink this to redis.
+		//5 - apply machine learning model
+		SingleOutputStreamOperator<TransactionModel> transactionModelsWithMlApplied = transactionModels.map(new ApplyMLMapper(v));
+		//6 - Convert to json and encrypt if is nescessary. Output Tuple2<String,String>
+		SingleOutputStreamOperator<Tuple2<String,String>> tuples = transactionModelsWithMlApplied.map(new JsonEncryptMapper(initialTime, aes));
+		//7 - sink this to redis.
 		tuples.addSink(new RedisSink<Tuple2<String, String>>(conf, new RedisExampleMapper())).name("RedisSink");
 		
 		
@@ -103,6 +102,58 @@ public class TransactionProcesser {
 	    public String getValueFromData(Tuple2<String, String> data) {
 	        return data.f1;
 	    }
+	}
+	
+	public static class HourlyExtractor extends BoundedOutOfOrdernessTimestampExtractor<TransactionModel> {
+
+		public HourlyExtractor(Time maxOutOfOrderness) {
+			super(maxOutOfOrderness);
+			
+		}
+
+		@Override
+		public long extractTimestamp(TransactionModel element) {
+			return Time.hours(element.getStep()-1).toMilliseconds();
+		}
+		
+	}
+	
+	/**
+	 * Sum the amount for a key and a time window
+	 * @author viniciuspc
+	 *
+	 */
+	public static class SumAggregate implements AggregateFunction<TransactionModel, Tuple2<String,Double>, Tuple2<String,Double>> {
+
+		@Override
+		public Tuple2<String, Double> createAccumulator() {
+			return new Tuple2<String, Double>("", 0.0);
+		}
+
+		@Override
+		public Tuple2<String, Double> add(TransactionModel value, Tuple2<String, Double> accumulator) {
+			double sum = accumulator.f1;
+			if(accumulator.f1 > 0.0 && value.getAmount() > accumulator.f1) {
+				//is fraud will mark transaction as a fraud;
+				value.setFraud(true);
+			} else {
+				sum+=value.getAmount();
+			}
+			return new Tuple2<String, Double>(value.getNameOrig(), sum);
+		}
+
+		@Override
+		public Tuple2<String, Double> getResult(Tuple2<String, Double> accumulator) {
+			return accumulator;
+		}
+
+		@Override
+		public Tuple2<String, Double> merge(Tuple2<String, Double> a, Tuple2<String, Double> b) {
+			return new Tuple2<String, Double>(a.f0, a.f1+b.f1);
+		}
+
+		
+		
 	}
 }
 
