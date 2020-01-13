3870b901521a0b4570bc001f1ca300e83ae51795
viniciuspc <viniciuspc@vinicius-pc>
2020-01-13 19:19:07 +0000

    Changes to consider only the start of orig and dest as country id

--- FlinkAndRedis/src/main/java/pt/archifeed/flink/GenerateEnrichdata.java
+++ FlinkAndRedis/src/main/java/pt/archifeed/flink/GenerateEnrichdata.java
@@ -13,27 +13,24 @@ import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolC
 import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommand;
 import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommandDescription;
 import org.apache.flink.streaming.connectors.redis.common.mapper.RedisMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class GenerateEnrichdata {
-	private static Logger LOG = LoggerFactory.getLogger(GenerateEnrichdata.class);
 	
 	public static void main(String[] args) throws Exception {
 		
 		
 		ParameterTool params = ParameterTool.fromArgs(args);
-		//FlinkJedisPoolConfig conf = new FlinkJedisPoolConfig.Builder().setHost("172.17.0.3").setPort(6379).build();
+		
 		FlinkJedisPoolConfig conf = new FlinkJedisPoolConfig.Builder().setHost(params.get("enrichment-host")).setPort(6379).build();
 		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 		
 		int index = 3;
-		if(params.get("localtion").toLowerCase().equals("dest")) {
+		if(params.get("location") != null && params.get("location").toLowerCase().equals("dest")) {
 			index = 6;
 		}
 		
-		long now = System.currentTimeMillis();
-		//DataStream<String> text = env.readTextFile("text-files/transactions.csv");
+		
+		
 		DataStream<String> text = env.readTextFile(params.get("file-path"));
 		
 		SingleOutputStreamOperator<Tuple2<String,String>> tuples = text.map(new MapFunctionEnrichment(index));
@@ -42,9 +39,7 @@ public class GenerateEnrichdata {
 		
 		
 		env.execute();
-		long time = System.currentTimeMillis()-now;
-		LOG.info("Total time: "+time+" Number of transactions: 24.000.000 Avarage time per transaction: "+(time/24000000.0));
-		System.out.println("Total time: "+time+" Number of transactions: 24.000.000 Avarage time per transaction: "+(time/24000000.0));
+		
 	}
 	
 	public static class RedisExampleMapper implements RedisMapper<Tuple2<String, String>>{
@@ -80,9 +75,14 @@ public class GenerateEnrichdata {
 			String[] fields = value.split(",");
 			String[] countries = new String[]{"Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"};
 			
-			return new Tuple2<String, String>(fields[index],countries[new Random().nextInt(countries.length)]);
+				
+				if(fields[index] == null || fields[index].length() < 4) {
+					return new Tuple2<String, String>(fields[index],countries[new Random().nextInt(countries.length)]);
+				} else {
+					return new Tuple2<String, String>(fields[index].substring(0, 4),countries[new Random().nextInt(countries.length)]);
+				}
 		}
-		
+
 	}
 }
 
